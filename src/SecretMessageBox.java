565543d61295ac9dc045a1e697901fbf1035a66a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-31 17:53:54 -0700

    secret message box

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -16,16 +16,35 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set a password in a String variable
-	
+	String password = "dave";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String message = JOptionPane.showInputDialog("enter a secret code here");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+JOptionPane.showMessageDialog(null, " you can only see the secret message "
+		+ "\nif you can guess the passcode");
 	// 4. If their guess matches the password, show them the secret message
 
+String guess = JOptionPane.showInputDialog("Password?");
+if (guess.equals(password)) {
+	JOptionPane.showMessageDialog(null,message);	
+}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+else {
+	JOptionPane.showMessageDialog(null,"INTRUDER");
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
