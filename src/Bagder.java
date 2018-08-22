cdbdc2bb6652cfb71d616c7b6f065b61ed4d6e76
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-21 17:45:59 -0700

    badger banana

---
+++ src/Bagder.java
@@ -0,0 +1,33 @@
+
+public class Bagder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub);
+		for (int i = 0; i < 4; i++) {	
+			for (int l = 0; l < 3; l++) {
+			for (int f = 0; f <10 ; f++) {
+				System.out.println("badger");	
+			}	
+
+			for (int j = 0; j < 4; j++)
+			{
+				System.out.println("mushroom");	
+			}	
+		}	
+
+		for (int j = 0; j < 2; j++)
+		{
+	System.out.println("snake");	
+}
+			
+		}
+		
+
+
+	
+
+			
+		
+	}
+
+}
---
+++ src/extra/banana.java
@@ -0,0 +1,17 @@
+package extra;
+
+public class banana {
+public static void main(String[] args) {
+	for (int i = 0; i < 1; i++) {
+		System.out.println("ice cream ice cream banana");
+		
+	}
+	
+	
+}
+	
+	
+	
+	
+	
+}
